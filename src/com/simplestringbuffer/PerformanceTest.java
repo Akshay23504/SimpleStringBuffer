a07abefe4d2eb2ad75d0cbed0b94dcbb13712eaa
akshay23504@gmail.com <akshay23504@gmail.com>
2015-12-29 00:13:53 +0530

    Added gitignore and concat method for string test

--- src/com/simplestringbuffer/PerformanceTest.java
+++ src/com/simplestringbuffer/PerformanceTest.java
@@ -1,27 +1,28 @@
 package com.simplestringbuffer;
 
-/**
- * Created by Akshay on 28-12-2015.
- */
-public class PerformaceTest {
+public class PerformanceTest {
 
     static long startTime, stopTime;
     static String string;
     static String s1 = "String One";
     static String s2 = "String Two";
     static String s3 = "String Three";
-    static String s;
+    static String s = "";
 
     public static void main(String[] args) {
         startTime = System.nanoTime();
         StringConcatenation();
         stopTime = System.nanoTime();
+        System.out.println(s);
         System.out.println(stopTime - startTime + "\n" + (stopTime - startTime) / 1000000000.0);
 
-        SimpleStringBuffer simpleStringBuffer = new SimpleStringBuffer(s1);
+        SimpleStringBuffer simpleStringBuffer = new SimpleStringBuffer("");
         startTime = System.nanoTime();
-        simpleStringBuffer.append(s2);
-        simpleStringBuffer.append(s3);
+        for(long i = 0; i < 2000; i++) {
+            simpleStringBuffer.append(s1);
+            simpleStringBuffer.append(s2);
+            simpleStringBuffer.append(s3);
+        }
         string = simpleStringBuffer.toString();
         stopTime = System.nanoTime();
         System.out.println(string);
@@ -29,8 +30,9 @@ public class PerformaceTest {
     }
 
     private static void StringConcatenation() {
-        for(int i = 0; i < 100; i++) {
-            s = s1 + s2 + s3;
+        for(long i = 0; i < 2000; i++) {
+            s = s + s1 + s2 + s3;
+            //s = s.concat(s1).concat(s2).concat(s3);
         }
     }
 
